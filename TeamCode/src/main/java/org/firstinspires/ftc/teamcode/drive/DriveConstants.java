79b376e2a4d32e4948a263445f3ddf13fa59c0d5
FatherBrown117 <levigbrown2@gmail.com>
2024-02-15 15:09:40 -0600

    Finished tuning RoadRunner!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15.5; // in
+    public static double TRACK_WIDTH = 15.62; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0042189717251797645;
-    public static double kA = 0.00000001;
-    public static double kStatic = 1.3483979830719992;
+    public static double kV =  0.016; //0.00001;
+    public static double kA = 0.0029;
+    public static double kStatic = 0.07965;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 34.51952;
     public static double MAX_ACCEL = 52.48291908330528;
-    public static double MAX_ANG_VEL = Math.toRadians(194.00321032258066);
+    public static double MAX_ANG_VEL = Math.toRadians(210.2080);
     public static double MAX_ANG_ACCEL = Math.toRadians(194.00321032258066);
 
     /*
